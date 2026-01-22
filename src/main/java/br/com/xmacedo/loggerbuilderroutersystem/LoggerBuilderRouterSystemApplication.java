ab13099d2c27dd1c64477fad762ee70e9845ca8b
Felipe Macedo <felipe.x.macedo@gmail.com>
2026-01-22 18:12:12 +0000

    Added initial structure

---
+++ src/main/java/br/com/xmacedo/loggerbuilderroutersystem/LoggerBuilderRouterSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.xmacedo.loggerbuilderroutersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerBuilderRouterSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggerBuilderRouterSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/xmacedo/loggerbuilderroutersystem/LoggerBuilderRouterSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xmacedo.loggerbuilderroutersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggerBuilderRouterSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
